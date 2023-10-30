18bbb2d288d20c08940ee37fa3709457699fe4fb
tpasfieldERAU <111624637+tpasfieldERAU@users.noreply.github.com>
2023-10-30 17:18:32 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by TJ on 10/30/2023.
+//
+
+#include <iostream>
+
+/* Define constants such as resolution and escape limit.
+ * This is only here during initial development. Once systems are refined
+ * there will be variable values for these and user input will be taken
+ * during operation. */
+
+#define XRES 144
+#define YRES 145
+#define ESC 512
+
+using namespace::std;
+
+int main(){
+
+}
